c7094aba8bc7e00b6fe8d4c0766f698528b10bd9
CaioBoh <caio.bohlhalter@unesp.br>
2023-09-21 03:12:33 -0300

    Funções Inserir e Remover, além de ajustar a Carrega Arquivo e outras pequenas alterações

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Definição da estrutura de dados
+
+struct RegistroArquivoRemove { //Proveniente do arquivo Remove.bin
+    char cod_cli[12];
+    char cod_vei[8];
+};
+
 struct RegistroLocacao {
     char CodCli[12];  // +1 para o caractere nulo ('\0')
     char CodVei[8];   // +1 para o caractere nulo ('\0')
@@ -11,6 +18,8 @@ struct RegistroLocacao {
 };
 
 struct RegistroLocacao registros[100];  // TODO: Mudar para dentro da função, retornando do carrega arquivo
+struct RegistroArquivoRemove remover[100]; //Verificar se vai fazer Array tamanho fixo ou Malloc
+
 
 //Função para verificar se o aqruivo existe e caso não, ele irá criar e inicializar com -1, indicando que não há espaços vazios no arquivo
 void criaArquivo(){
@@ -20,9 +29,8 @@ void criaArquivo(){
         return;
     }
     else{
-        int offset = -1;
         arquivo = fopen("registro.bin", "w+b");
-        fwrite(&offset, 1, sizeof(int), arquivo);
+        fwrite("-1", 1, sizeof(char), arquivo);
         fclose(arquivo);
     }
 }
@@ -30,8 +38,8 @@ void criaArquivo(){
 // Função para inserção de um registro no final do arquivo TODO: verificar tamanho disponivel antes de inserir
 void inserirRegistro(struct RegistroLocacao registroInserir) {
 
-    FILE *arquivo = fopen("registro.bin", "a+b");
-    char tamanho, ponteiro;
+    FILE *arquivo = fopen("registro.bin", "rb+");
+    char tamanho, offset;
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
@@ -40,12 +48,18 @@ void inserirRegistro(struct RegistroLocacao registroInserir) {
 
     tamanho = 18 + strlen(registroInserir.NomeCliente) + strlen(registroInserir.NomeVeiculo) + 4;
 
-    fread(&ponteiro, sizeof(int), 1, arquivo);
-    if(ponteiro == -1){
+    fread(&offset, sizeof(char), 1, arquivo);
+    if(offset == -1){
         arquivo = fopen("registro.bin", "a+b");
     }
     else{
-        fread(&ponteiro, sizeof(char), 1, arquivo);
+        offset = (tamanho);
+        if(offset == -1){
+            arquivo = fopen("registro.bin", "a+b");
+        }
+        else{
+            fseek(arquivo, offset, SEEK_SET);
+        }
     }
 
     fwrite(&tamanho, 1, sizeof(char), arquivo);
@@ -64,7 +78,43 @@ void inserirRegistro(struct RegistroLocacao registroInserir) {
 }
 
 // Função para remover os registros TODO: precisamos remover o registro baseado na chave primaria (CodCli + CodVei)
-void removerRegistro() {
+void removerRegistro(char CodVei[], char CodCli[]) {
+    FILE *arquivo = fopen("registro.bin", "rb+");
+
+    char CodVeiRegistro[8], CodCliRegistro[12], tamanhoRegistro;
+    bool RegistroEncontrado = false;
+    int offset;
+
+    fread(&offset, sizeof(int), 1, arquivo); //Lendo o offset inicial
+    while(fread(&tamanhoRegistro, sizeof(char), 1, arquivo)){
+        
+        fread(&CodCliRegistro, sizeof(char), 8, arquivo);
+        fseek(arquivo, 1, SEEK_CUR);
+        fread(&CodVeiRegistro, sizeof(char), 12, arquivo);
+
+        if((CodVei != CodVeiRegistro) && (CodCli != CodCliRegistro)){
+            fseek(arquivo, tamanhoRegistro - 21, SEEK_CUR);
+        }
+        else{
+            RegistroEncontrado = true;
+            break;
+        }
+    }
+
+    if(RegistroEncontrado){
+
+        fseek(arquivo, -21, SEEK_CUR);
+        offset = ftell(arquivo) - 1;
+        offset = adicionaOffset(offset);
+        
+        fwrite("*", sizeof(char), 1, arquivo);
+        fwrite(offset, sizeof(int), 1, arquivo);
+        printf("\n---Registro Removido com sucesso---\n\n");
+        
+    }
+    else{
+        printf("\n---Registro Não Encontrado---\n\n");
+    }
 
 }
 
@@ -75,36 +125,111 @@ void compactarArquivo() {
 
 // Vai abrir o arquivo insere.bin e carregar todos os registros em um vetor de structs
 void carregarArquivo() {
+    //Fazendo primeiro para Insere.bin
     FILE *arquivo = fopen("insere.bin", "rb");
+    int quantidadeRegistros = 0;
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
         return;
     }
 
-    int numRegistros = 0;
+    while(fread(&registros[quantidadeRegistros], sizeof(struct RegistroLocacao), 1, arquivo)){
+        quantidadeRegistros++;
+    }
+    fclose(arquivo);
 
-    while (fread(&registros[numRegistros], sizeof(struct RegistroLocacao), 1, arquivo) == 1) {
-        numRegistros++;
+    //Fazendo para Remove.bin
+    arquivo = fopen("remove.bin", "rb");
+    quantidadeRegistros = 0;
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    while(fread(&remover[quantidadeRegistros], sizeof(struct RegistroArquivoRemove), 1, arquivo)){
+        quantidadeRegistros++;
     }
     fclose(arquivo);
 }
 
+void lerArquivo() {
+    FILE *arquivo = fopen("registro.bin", "rb");
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    fclose(arquivo);
+}
+
+//Adiciona um novo Offset a "pilha"
+int adicionaOffset(int posicaoRemovido){
+    FILE *arquivo = fopen("registro.bin", "rb+");
+    int offset = 0, offsetAnterior;
+
+    while(offset != -1){
+        offsetAnterior = offset;
+        fseek(arquivo, offset + 2, SEEK_SET); //Anda o tamanho +1 do tamanho +1 do *
+        fread(&offset, sizeof(int), 1, arquivo);
+    }
+
+    rewind(arquivo);
+    fwrite(posicaoRemovido, sizeof(int), 1, arquivo);
+    return offsetAnterior;
+
+}
+ 
+//Busca um espaço para inserção de registro e atualiza a "pilha" de Offsets
+int buscaEspaço(char tamanho){
+    
+    FILE *arquivo = fopen("registro.bin", "rb+");
+    int offset = 0, offsetAnterior;
+    char espaçoLivre = 0;
+
+    while((offset != -1) && (espaçoLivre < tamanho)){
+        offsetAnterior = offset;
+
+        fseek(arquivo, offset, SEEK_SET);
+        fread(espaçoLivre, sizeof(char), 1, arquivo);
+        fseek(arquivo, 1, SEEK_CUR);
+        fread(&offset, sizeof(int), 1, arquivo);
+
+    }
+
+    if(offset == -1){
+        return -1;
+    }
+    else{
+        //Atualiza o offset do começo
+        rewind(arquivo);
+        fwrite(offsetAnterior, sizeof(int), 1, arquivo);
+        return offsetAnterior;
+    }
+
+
+}
 // Função para administrar o menu
-void menu(FILE *file){
-    int resposta;
+void menu(){
+    int resposta, posicao;
+
     do{
         printf("Escolha uma opção:\n");
-        printf("(1)Inserir\n(2)Remover\n(3)Compactar\n(4)Carregar Arquivo\n(5)Sair\n");
+        printf("(1)Inserir\n(2)Remover\n(3)Compactar\n(4)Carregar Arquivos\n(5)Ler Arquivo\n(6)Sair\n");
         scanf("%d", &resposta);
 
         switch (resposta) {
             case 1:
-                system("clear");
-                inserirRegistro(registros[1]);  // Usando um registro de exemplo para testar, mudar depois
-                break;
+                //system("clear");
+                printf("Qual o registro deseja inserir?\n");
+                scanf("%d", &posicao);
+                inserirRegistro(registros[posicao]);
             case 2:
-                removerRegistro();
+                printf("Qual o registro deseja remover?\n");
+                scanf("%d", &posicao);
+                removerRegistro(remover[posicao].cod_vei, remover[posicao].cod_cli);
                 break;
             case 3:
                 compactarArquivo();
@@ -112,23 +237,22 @@ void menu(FILE *file){
             case 4:
                 carregarArquivo();
                 break;
+            case 5:
+                lerArquivo();
+                break;
+            case 6:
+            break;
             default:
                 printf("Valor inválido!\n");
         }
     }
-    while(resposta != 5);
+    while(resposta != 6);
 }
 
 // Função main
 int main() {
-    FILE *file = fopen("registros.bin", "a+b"); // Abre o arquivo para leitura e escrita binária
-
-    if (file == NULL) {
-        perror("Erro ao abrir o arquivo");
-        return 1;
-    }
  
-    menu(file);
-    fclose(file);
+    criaArquivo();
+    menu();
     return 0;
 }
