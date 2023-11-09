cdadaeebc0d49ea8368d5c2dc942e28e6703b9f0
renanleite <renan.vbleite@gmail.com>
2023-11-08 21:51:58 -0300

    Adicionando defines, struct para a arvore b, funcoes para administrar arvore

--- projeto-3/projeto3.c
+++ projeto-3/projeto3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define MAXKEYS 3
+#define NIL (-1)
+#define NOKEY '@'
+#define NO 0
+#define YES 1
+
+struct BTPage{
+    short keycount; // number of keys in page
+    char key[MAXKEYS]; // the actual keys
+    short child[MAXKEYS+1]; // RRNs dos filhos
+};
+
+#define PAGESIZE sizeof(struct BTPage)
 
 struct RegistroBusca {  //Proveniente do arquivo busca.bin
     char cod_cli[12];
@@ -12,7 +27,7 @@ struct RegistroLocacao {
     char CodVei[8];   // +1 para o caractere nulo ('\0')
     char NomeCliente[50];  // +1 para o caractere nulo ('\0')
     char NomeVeiculo[50];  // +1 para o caractere nulo ('\0')
-    int NumeroDias;
+    char NumeroDias[4];
 };
 
 struct RegistroLocacao registrosInsercao[100];  // para armazenar o arquivo insere.bin
@@ -70,9 +85,73 @@ void carregaArquivos(){
     fclose(arquivo);
 }
 
+// TODO: FUNCOES PARA A ARVORE, PRECISA TESTAR SE ESTA FUNCIONAL
+
+bool btOpen(FILE *fileBTree) {
+    if ((fileBTree = fopen("BTree.bin", "r+b")) == NULL) {
+        return false;
+    }
+    return true;
+}
+
+short getRoot(FILE *fileBTree) {
+    short root;
+    fseek(fileBTree, 0, SEEK_SET);
+    if(!(fread(&root, sizeof(short), 1, fileBTree))) {
+        printf("Erro ao ler raiz");
+        exit(1);
+    }
+    return root;
+}
+
+short getpageRRN(FILE *fileBTree) {
+    short addr;
+    fseek(fileBTree, 0, SEEK_END);
+    addr = ftell(fileBTree) - sizeof(short);
+    return ((short) addr / PAGESIZE);
+}
+
+void pageInit(struct BTPage *p_page) {
+    // TODO
+}
+
+int createTree(FILE *fileBTree) {
+    int menosum = NIL;
+
+    if ((fileBTree = fopen("BTree.bin", "w+b")) == NULL) {
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    fseek(fileBTree, 0, SEEK_SET);
+    fwrite(&menosum, sizeof(int), 1, fileBTree); // header = -1
+
+    return 0;
+    // TODO: fazer o retorno parar criar a raiz (create_root)
+}
+
+void inserirRegistro(FILE *registros, FILE *fileBTree) {
+
+    // TODO: terminar funcao inserir
+    int posicao, root;
+    printf("Qual registro deseja inserir?\n");
+    scanf("%d", &posicao);
+    posicao --; // para converter, comecando em zero
+
+    // Inserindo o registro no final do arquivo principal:
+    fseek(registros, 0, SEEK_END);
+    fwrite(&registrosInsercao[posicao], sizeof(struct RegistroLocacao), 1, registros);
+
+    if(btOpen(fileBTree))
+        root = getRoot(fileBTree);
+
+    else
+        root = createTree(fileBTree);
+}
+
 int main () {
 
-    FILE *fileRegistros;
+    FILE *fileRegistros, fileBTree;
     int resposta;
 
     // Se o arquivo principal não existe, criar o arquivo para escrita:
@@ -95,6 +174,7 @@ int main () {
 
         switch (resposta) {
             case 1:
+                inserirRegistro(fileRegistros, &fileBTree);
                 break;
             case 2:
                 break;
